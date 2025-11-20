20fe74e965df50dbf50b3a39a34a9e830e02f77c
sofomir <sofomir@gmail.com>
2025-11-20 12:35:35 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//hiding/showing menu on mobile
+const burgerLine = document.querySelector('.nav-burger-line')
+const nav = document.querySelector('nav')
+const navBurger = document.querySelector('.nav-burger')
+const logoBtn = document.querySelector('.logo a')
+const navItems = document.querySelectorAll('nav a')
+const header = document.querySelector('header')
+
+navBurger.addEventListener('click', () => {
+	burgerLine.classList.toggle('nav-burger-x')
+	nav.classList.toggle('nav-main-show')
+})
+
+const closeMenu = () => {
+	if (nav.classList.contains('nav-main-show')) {
+		burgerLine.classList.remove('nav-burger-x')
+		nav.classList.remove('nav-main-show')
+	}
+}
+
+;[...navItems, logoBtn].forEach((element) => {
+	element.addEventListener('click', closeMenu)
+    // headerHide.classList.add('header-hide')
+})
+
+//hiding/showing header when scrolling
+const headerHide = document.querySelector('header')
+let lastScroll = 0
+window.addEventListener('scroll', () => {
+	const currentScrollY = window.scrollY
+	if (currentScrollY > lastScroll) {
+		if (!nav.classList.contains('nav-main-show')) headerHide.classList.add('header-hide')
+	} else {
+		headerHide.classList.remove('header-hide')
+	}
+	lastScroll = currentScrollY
+})
